41fd1a82797895573db2d4eecdb211b2b8037efd
yeon <xxyeon129@gmail.com>
2024-07-24 23:10:07 +0900

    NextJS Code: edit metadata

--- Next/Code/app/about-us/page.tsx
+++ Next/Code/app/about-us/page.tsx
@@ -1,4 +1,6 @@
-import Navigation from '../../components/navigation';
+export const metadata = {
+  title: 'About us',
+};
 
 export default function AboutUs() {
   return (
--- Next/Code/app/not-found.tsx
+++ Next/Code/app/not-found.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: 'Not found',
+};
+
 export default function NotFound() {
   return <h1>NOT FOUND!</h1>;
 }
