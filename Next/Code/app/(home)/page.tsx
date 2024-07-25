7c6d9b2bb1a8c124f74031a99bab204a12b08f98
yeon <xxyeon129@gmail.com>
2024-07-25 09:56:03 +0900

    NextJS Code: server side data fetching

---
+++ Next/Code/app/(home)/page.tsx
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: 'Home',
+};
+
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+
+  return <div>{JSON.stringify(movies)}</div>;
+}
