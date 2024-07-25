72f6cb767742b02fe23a928c58bf093fa8ce96e0
yeon <xxyeon129@gmail.com>
2024-07-25 12:03:40 +0900

    NextJS Code: error handling

--- Next/Code/components/movie-videos.tsx
+++ Next/Code/components/movie-videos.tsx
@@ -1,8 +1,10 @@
 import { API_URL } from '../app/(home)/page';
 
 async function getVideos(id: string) {
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
   console.log(`Fetching videos: ${Date.now()}`);
+
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  // throw new Error('something broke...');
 
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
