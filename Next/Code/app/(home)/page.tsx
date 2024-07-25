fcdd491ee8e4b98d2adddf1abf189eba1ec342ef
yeon <xxyeon129@gmail.com>
2024-07-25 10:37:34 +0900

    NextJS Code: parallel requests

--- Next/Code/app/(home)/page.tsx
+++ Next/Code/app/(home)/page.tsx
@@ -1,13 +1,13 @@
+import Link from 'next/link';
+
 export const metadata = {
   title: 'Home',
 };
 
-const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-
-  const response = await fetch(URL);
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 }
@@ -15,5 +15,13 @@ async function getMovies() {
 export default async function HomePage() {
   const movies = await getMovies();
 
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
--- Next/Code/app/movies/[id]/page.tsx
+++ Next/Code/app/movies/[id]/page.tsx
@@ -1,3 +1,28 @@
-export default function MovieDetail({ params: { id } }: { params: { id: string } }) {
-  return <h1>Movie {id}</h1>;
+import { API_URL } from '../../(home)/page';
+
+async function getMovie(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log(`Fetching movies: ${Date.now()}`);
+
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log(`Fetching videos: ${Date.now()}`);
+
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
+  console.log('=================================');
+  console.log('start fetching');
+
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+
+  console.log('end fetching');
+
+  return <h1>{movie.title}</h1>;
 }
