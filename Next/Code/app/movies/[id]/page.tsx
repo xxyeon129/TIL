d1eb5007d4d91d76a9f8752b056a725cde364f62
yeon <xxyeon129@gmail.com>
2024-07-26 23:31:17 +0900

    NextJS Code: movie videos

--- Next/Code/app/movies/[id]/page.tsx
+++ Next/Code/app/movies/[id]/page.tsx
@@ -5,12 +5,12 @@ import MovieVideos from '../../../components/movie-videos';
 export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
   return (
     <div>
-      <Suspense fallback={<h1>Loading movie videos</h1>}>
-        <MovieVideos id={id} />
-      </Suspense>
-
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
+      </Suspense>
+
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
       </Suspense>
     </div>
   );
--- Next/Code/components/movie-videos.tsx
+++ Next/Code/components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from '../app/(home)/page';
+import styles from '../styles/movie-videos.module.css';
 
 async function getVideos(id: string) {
   console.log(`Fetching videos: ${Date.now()}`);
@@ -12,5 +13,17 @@ async function getVideos(id: string) {
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
