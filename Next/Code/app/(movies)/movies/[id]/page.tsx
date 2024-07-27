eea2c689c3bc4a37322f834ca850bdec4af978a5
yeon <xxyeon129@gmail.com>
2024-07-27 11:38:01 +0900

    NextJS Code: move movies code into (movies) folder

--- Next/Code/app/(movies)/movies/[id]/page.tsx
+++ Next/Code/app/(movies)/movies/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
-import MovieInfo from '../../../components/movie-info';
-import MovieVideos from '../../../components/movie-videos';
+import MovieInfo from '../../../../components/movie-info';
+import MovieVideos from '../../../../components/movie-videos';
 
 export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
   return (
