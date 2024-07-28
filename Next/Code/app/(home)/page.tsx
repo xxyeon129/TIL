086279578e2c9029ce1965ff99673a121bd692c1
yeon <xxyeon129@gmail.com>
2024-07-28 22:36:03 +0900

    NextJS Code: separate constant

--- Next/Code/app/(home)/page.tsx
+++ Next/Code/app/(home)/page.tsx
@@ -1,11 +1,10 @@
+import { API_URL } from '../../constants';
 import Movie from '../../components/movie';
 import styles from '../../styles/home.module.css';
 
 export const metadata = {
   title: 'Home',
 };
-
-export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
 async function getMovies() {
   const response = await fetch(API_URL);
--- Next/Code/components/movie-info.tsx
+++ Next/Code/components/movie-info.tsx
@@ -1,5 +1,5 @@
+import { API_URL } from '../constants';
 import styles from '../styles/movie-info.module.css';
-import { API_URL } from '../app/(home)/page';
 
 export async function getMovie(id: string) {
   // await new Promise((resolve) => setTimeout(resolve, 5000));
