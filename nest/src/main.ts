736c1f174a20ff28bda33221821474d10e75c49c
seungyeon <yeon1209@beautifulstore.org>
2025-03-17 17:43:10 +0900

    [Nest] Feat: signup test

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 8080);
 }
 void bootstrap().catch((error) => {
   console.error(error);
