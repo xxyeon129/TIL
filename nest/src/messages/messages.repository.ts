da02f377afdc1c931e7cc1399d1c5f3896f64136
yeon <xxyeon129@gmail.com>
2025-04-13 16:41:53 +0900

    [Nest] Feat: add repository

---
+++ nest/src/messages/messages.repository.ts
@@ -0,0 +1,39 @@
+// Repository: 데이터베이스 조작 로직을 캡슐화
+// access DB(or mockdata)
+
+import { readFile, writeFile } from 'fs/promises'; // DB(or mockdata)의 콘텐츠를 읽거나 씀
+
+interface Message {
+  id: number;
+  content: string;
+}
+
+interface Messages {
+  [key: number]: Message;
+}
+
+export class MessagesRepository {
+  async findOne(id: string): Promise<Message | undefined> {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages: Messages = JSON.parse(contents);
+
+    return messages[parseInt(id)];
+  }
+
+  async findAll(): Promise<Messages> {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages: Messages = JSON.parse(contents);
+
+    return messages;
+  }
+
+  async create(content: string): Promise<void> {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages: Messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
